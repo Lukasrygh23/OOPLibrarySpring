c3f07b32643a0b01de83141c546801e1792f962c
LukasR23 <Lukas@jazzcatz.com>
2021-11-16 17:02:07 +0000

    This is just to get the first proper commit in there so I have folders to stare at. Will swap to dev after.

---
+++ src/main/java/com/qa/LukasSpring/LukasLibraryOopApplication.java
@@ -0,0 +1,13 @@
+package com.qa.LukasSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukasLibraryOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukasLibraryOopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/LukasSpring/LukasLibraryOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.LukasSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukasLibraryOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
